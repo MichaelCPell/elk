47da3321ece548037185bfdeba94195621803fe8
Michael Pell <MichaelCPell@gmail.com>
2013-04-05 11:36:53 -0400

    Add Finalize

--- elk.rb
+++ elk.rb
@@ -14,6 +14,7 @@ ENV['RACK_ENV'] ||= "development"
 case ENV['RACK_ENV']
 when "production"
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  DataMapper.finalize
 when "development"
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, 'postgres://postgres@localhost/development_elk')
