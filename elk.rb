d98f5151f33ce12add21efccdb0af40585d19d99
Michael Pell <MichaelCPell@gmail.com>
2013-04-05 10:37:26 -0400

    Adds Datamapper Config to Elk.rb

--- elk.rb
+++ elk.rb
@@ -13,8 +13,9 @@ RACK_ENV ||= "development"
 
 case RACK_ENV
 when "production"
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 when "development"
-   DataMapper::Logger.new($stdout, :debug)
-   DataMapper.setup(:default, 'postgres://postgres@localhost/development_elk')
-   DataMapper.finalize
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'postgres://postgres@localhost/development_elk')
+  DataMapper.finalize
 end
